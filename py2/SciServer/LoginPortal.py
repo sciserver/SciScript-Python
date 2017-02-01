f62656110d10050a841c827026d80e75ddb83c3e
mtaghiza <mtaghiza@jhu.edu>
2017-02-01 15:51:56 -0500

    Added the LoginPortal namespace, which wraps the methods inside the Authentication namespace. Only to keep the old code still working. Will have to deprecate this, though.

---
+++ py2/SciServer/LoginPortal.py
@@ -0,0 +1,48 @@
+__author__ = 'mtaghiza, gerard'
+#Python v3.4
+
+import json
+import sys
+import requests
+import os.path
+
+import SciServer.Authentication;
+
+class KeystoneUser:
+    id = "KeystoneID"
+    userName = "User Name"
+
+def getKeystoneUserWithToken(token):
+  return SciServer.Authentication.getKeystoneToken();
+
+def login(UserName, Password):
+    return SciServer.Authentication.login(UserName, Password);
+
+def getToken():
+    """
+    Retrieve the token stored in the docker container. If not found there, will look at system argument --ident.
+    Will only work when running on docker.
+    """
+    # This code block defined your token and makes it available as a
+#   system variable for the length of your current session.
+#
+# This will usually be the first code block in any script you write.
+    return SciServer.Authentication.getToken();
+
+def identArgIdentifier():
+    return SciServer.Authentication.identArgIdentifier();
+
+def getKeystoneToken():
+    """
+    Returns the users keystone token passed into the python instance with the --ident argument.
+    """
+    return SciServer.Authentication.getKeystoneToken();
+
+
+def setKeystoneToken(token):
+    """
+    Set the token as the --ident argument
+    :param token:
+    :return:
+    """
+    SciServer.Authentication.setKeystoneToken();
