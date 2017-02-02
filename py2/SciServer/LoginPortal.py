dd25b687ef09d097ff20e1303cb717d3a67f48e9
mtaghiza <mtaghiza@jhu.edu>
2017-02-02 17:34:00 -0500

    added deprecated tag to LoginPortal methods

--- py2/SciServer/LoginPortal.py
+++ py2/SciServer/LoginPortal.py
@@ -5,19 +5,39 @@ import json
 import sys
 import requests
 import os.path
-
+import warnings
 import SciServer.Authentication;
 
+
+def deprecated(func):
+    '''This is a decorator which can be used to mark functions
+     as deprecated. It will result in a warning being emitted
+     when the function is used.'''
+    def new_func(*args, **kwargs):
+        warnings.warn("Call to deprecated function {}.".format(func.__name__),category = DeprecationWarning)
+        return func(*args, **kwargs)
+
+    new_func.__name__ = func.__name__
+    new_func.__doc__ = func.__doc__
+    new_func.__dict__.update(func.__dict__)
+    return new_func
+
+
+
+@deprecated
 class KeystoneUser:
     id = "KeystoneID"
     userName = "User Name"
 
+@deprecated
 def getKeystoneUserWithToken(token):
   return SciServer.Authentication.getKeystoneToken();
 
+@deprecated
 def login(UserName, Password):
     return SciServer.Authentication.login(UserName, Password);
 
+@deprecated
 def getToken():
     """
     Retrieve the token stored in the docker container. If not found there, will look at system argument --ident.
@@ -29,16 +49,18 @@ def getToken():
 # This will usually be the first code block in any script you write.
     return SciServer.Authentication.getToken();
 
+@deprecated
 def identArgIdentifier():
     return SciServer.Authentication.identArgIdentifier();
 
+@deprecated
 def getKeystoneToken():
     """
     Returns the users keystone token passed into the python instance with the --ident argument.
     """
     return SciServer.Authentication.getKeystoneToken();
 
-
+@deprecated
 def setKeystoneToken(token):
     """
     Set the token as the --ident argument
