9c15d278f3ba9e79c9bafb36d67fc432a978ba39
mtaghiza <mtaghiza@jhu.edu>
2017-02-13 11:16:38 -0500

    Modified Config file connection URLs

--- py3/SciServer/Config_Sample.py
+++ py3/SciServer/Config_Sample.py
@@ -1,7 +1,8 @@
 # fix URLs
-CasJobsRESTUri = "http://scitest02.pha.jhu.edu/CasJobs/RestApi"
-AuthenticationURL = "http://scitest02.pha.jhu.edu/login-portal/keystone/v3/tokens"
-SciDriveHost = 'http://scitest09.pha.jhu.edu'
-SkyServerWSurl = 'http://scitest02.pha.jhu.edu/SkyserverWS'
-DataRelease = 'DR12'
+CasJobsRESTUri = "http://skyserver.sdss.org/CasJobs/RestApi"
+AuthenticationURL = "http://portal.sciserver.org/login-portal/keystone/v3/tokens"
+SciDriveHost = 'http://www.scidrive.org'
+SkyQueryUrl = 'http://voservices.net/skyquery/Api/V1'
+DataRelease = 'DR13'
+SkyServerWSurl = 'http://skyserver.sdss.org'
 executeMode = "release" # if executeMode = "debug", then strings (with useful information) will be printed on screen during the execution, wherever it is specified in the code. If executeMode = "release", then no string will be printed.
