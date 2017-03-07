8ea5e81e2a002aab1a42c973c4b5427626785dbb
mtaghiza <mtaghiza@jhu.edu>
2017-03-07 14:25:41 -0500

    Fixing the Update and ShowTags code

--- ShowSciServerTags.py
+++ ShowSciServerTags.py
@@ -13,8 +13,8 @@ sysPrint("\n---1) Updating local Git repository...\n\n")
 os.system("git tag -d $(git tag)") #deletes local tags
 os.system("git fetch --all") #fetches all remotes into local repo, including tags.
 os.system("git checkout master")
-os.system("git reset --hard origin/master") #resets the local master branch to what was just fetched.
-os.system("git clean -df") #removes all untracked files
+#os.system("git reset --hard origin/master") #resets the local master branch to what was just fetched.
+#os.system("git clean -df") #removes all untracked filesl
 
 sysPrint("\n--2) Listing available SciServer version Tags:\n\n")
 tags = os.popen("git tag").read().split("\n")
--- Update.py
+++ Update.py
@@ -10,12 +10,24 @@ def sysPrint(myString): # print string to screen, depending on python version
   else:
     print myString
 
+# Checks whether the library is being run within the SciServer-Compute environment. Returns True if the library is being run within the SciServer-Compute environment, and False if not.
+def isSciServerComputeEnvironment():
+    """
+    Checks whether the library is being run within the SciServer-Compute environment. Returns True if the library is being run within the SciServer-Compute environment, and False if not.
+    """
+    if os.path.isfile("/home/idies/keystone.token"):
+        return True
+    else:
+        return False
+
+
+
 sysPrint("\n---1) Updating local Git repository...\n\n")
 os.system("git tag -d $(git tag)") #deletes local tags
 os.system("git fetch --all") #fetches all remotes into local repo, including tags.
 os.system("git checkout master")
 os.system("git reset --hard origin/master") #resets the local master branch to what was just fetched.
-#os.system("git clean -df") #removes all untracked files
+os.system("git clean -df") #removes all untracked files
 
 
 if len(commandLineArguments) == 0:
@@ -32,9 +44,22 @@ else:
 os.chdir("./py2")
 
 sysPrint("\n---3) Building the SciServer package for Python 2...\n\n")
-os.system("python2 setup.py install")
+
+if isSciServerComputeEnvironment():
+  os.system("source activate py27")# activating python = python2 in anaconda
+  os.system("python setup.py install")
+  os.system("source activate root")# deactivating python = python2 in anaconda, now changing back to the anaconda default of python = python3
+else:
+  os.system("python2 setup.py install")
 
 os.chdir("../py3")
 
 sysPrint("\n---4) Building the SciServer package for Python 3...\n\n")
-os.system("python3 setup.py install")
+
+if isSciServerComputeEnvironment():
+  os.system("python setup.py install")# In anaconda, the default is python = python3
+else:
+  os.system("python3 setup.py install")
+
+
+
