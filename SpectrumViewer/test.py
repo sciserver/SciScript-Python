d5dcd1661dae509115b80ef32c638729bb6416f3
pryo <2012213057@bupt.edu.cn>
2017-10-29 17:41:27 -0400

    spectrum viewer v1
    the first prototype of spectrum viewer as part of sciserver. need to config together with sciserver

---
+++ SpectrumViewer/test.py
@@ -0,0 +1,9 @@
+import SpectrumViewer.ZObj as ZObj
+import SpectrumViewer.CoaddObj as CoaddObj
+import SpectrumViewer.WindowViewer as viewer
+fileName = 'example_lite.fits'
+fileSource = 'SDSS'
+viewer.view(fileName,fileSource)
+
+
+
